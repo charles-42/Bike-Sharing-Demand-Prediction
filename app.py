d50ed9b10c46b77b632b4c8c50bf4a824c4dee24
Sarthak Rana <sarthak6246@gmail.com>
2020-12-03 16:49:51 +0530

    removed beta keyword from config

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import main
 
 # App configs
-st.beta_set_page_config(
+st.set_page_config(
 page_title="Bike Sharing Demand Prediction",
 layout="centered",
 initial_sidebar_state="expanded",
